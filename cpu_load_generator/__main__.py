3ff028674969a175f5bbcba89dbfd2c0bd5d9934
Erathaowl <46474708+erathaowl@users.noreply.github.com>
2023-04-20 13:31:20 +0200

    fixed argument parsing

--- cpu_load_generator/__main__.py
+++ cpu_load_generator/__main__.py
@@ -16,8 +16,8 @@ def parse_args(parser):
     parser.add_argument('-p', '--path_to_profile_json', type=str, default="",
                         help='Path to CPU load profile json file. Using with any other arguments is disallowed!')
     parser.add_argument('-l', '--cpu_load', type=float, help='Cpu target load.')
-    parser.add_argument('-d', '--duration', type=int, help='Duration of the load in seconds. Should be higher than 0.')
-    parser.add_argument('-c', '--cpu_core', type=int, default=0,
+    parser.add_argument('-d', '--duration', type=int, help='Duration of the load in seconds. Should be 0 for infinite loop or higher than 0.')
+    parser.add_argument('-c', '--cpu_core', type=int, default=-1,
                         help='Select the CPU number on which generate the load. Default is 0.')
     parser.set_defaults()
     args = parser.parse_args()
@@ -39,14 +39,11 @@ def input_error_handler(args):
             raise ValueError('CPU load value must be provided')
 
         if not args.cpu_core < cpu_count:
-            args.print_help()
             raise ValueError('Core to load should not be higher than {}!'.format(cpu_count - 1))
-        if args.duration <= 0:
-            args.print_help()
-            raise ValueError('The load duration must be higher then 0!')
+        if args.duration < 0:
+            raise ValueError('The load duration must be 0 or higher value')
         if not 0 < args.cpu_load <= 1.0:
-            args.print_help()
-            raise ValueError('CPU load time should be the fraction of 1. Range (0; 1].')
+            raise ValueError('CPU load time should be the fraction of 1. Range [0..1].')
     else:
         input_arguments = sys.argv[1:]
         if ('-c' in input_arguments or '--cpu_core' in input_arguments) and \
